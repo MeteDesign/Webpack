98542fb9adb39f3a71749519a9a3f897456e5bb6
GoDotDotDot <chukui0202@gmail.com>
2017-07-25 23:19:07 +0800

    fixed bug

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ function nousefull () {
 }
 const Index = () => (
   <div>
-    <h3>Thanks for using Mete Design Webpack V3</h3>
+    <h3>Thanks for using Mete Design Webpack V3.</h3>
     <h4>Github: <a href='https://github.com/MeteDesign/Webpack'>MeteDesign/Webpack</a></h4>
+    <h4>Author: KUI CHU</h4>
     <img src={require('./assets/images/logo.png')} alt='logo' />
   </div>
 )
