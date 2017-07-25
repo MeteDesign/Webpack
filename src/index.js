fd796201af52f4d8c9d68020a34423ae0ef829e2
GoDotDotDot <chukui0202@gmail.com>
2017-07-26 00:16:12 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import {render} from 'react-dom'
 import './style.css'
-import './test.scss'
 
 function nousefull () {
   console.log('not usefull!')
   console.log('you will not see this if tree shaking is worked in production env')
 }
 const Index = () => (
-  <div>
-    <h3>Thanks for using Mete Design Webpack V3.</h3>
-    <h4>Github: <a href='https://github.com/MeteDesign/Webpack'>MeteDesign/Webpack</a></h4>
-    <h4>Author: KUI CHU</h4>
+  <div className='md-container'>
+    <h2>Mete Design Webpack V3</h2>
+    <h4>Thanks for using</h4>
+    <h4>Github Repo: <a target='_blank' href='https://github.com/MeteDesign/Webpack'>MeteDesign/Webpack</a></h4>
+    <h3>Contributors: <a target='_blank' href='https://github.com/godotdotdot'>KUI CHU</a></h3>
+    <hr />
     <img src={require('./assets/images/logo.png')} alt='logo' />
+    <h4>METE DESIGN GROUP</h4>
+    <img src={require('./assets/images/md.png')} alt='md' style={{width: 85, height: 85}} />
   </div>
 )
 
