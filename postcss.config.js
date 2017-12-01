14ffe222be741fbfe102cd484383f4ab52024797
GoDotDotDot <chukui0202@gmail.com>
2017-12-01 16:22:55 +0800

    update

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = (ctx) => ({
+  parser: ctx.parser ? 'sugarss' : false,
+  map: ctx.env === 'development' ? ctx.map : false,
+  plugins: {
+    'postcss-import': {},
+    'postcss-nested': {},
+    'postcss-plugin': {},
+    cssnano: ctx.env === 'production' ? {} : false,
+    autoprefixer: {}
+  }
+})
