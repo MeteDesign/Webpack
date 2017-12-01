00d57c2abf20825b5a25876a135e289d729c1084
GoDotDotDot <chukui0202@gmail.com>
2017-12-01 11:34:35 +0800

    合并入口

--- webpack.dll.js
+++ webpack.dll.js
@@ -6,9 +6,8 @@ const outputPath = path.resolve('dll')
 module.exports = {
   devtool: 'source-map',
   entry: {
-    react: ['react'],
-    react_dom: ['react-dom'],
-    react_router_dom: ['react-router-dom']
+    react_vendor: ['react', 'react-dom', 'react-router-dom']
+    // others: ['']
   },
 
   output: {
@@ -22,10 +21,11 @@ module.exports = {
 
   plugins: [
     new webpack.DllPlugin({
+      context: __dirname,
       // The path to the manifest file which maps between
       // modules included in a bundle and the internal IDs
       // within that bundle
-      path: 'dll/[name]_manifest.json',
+      path: path.join(__dirname, 'dll/[name]_manifest.json'),
       // The name of the global variable which the library's
       // require function has been assigned to. This must match the
       // output.library option above
